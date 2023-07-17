d6d03fa06b6d24833187f9f028a598ef4eb11b5f
gpanaretou <gpanaretou@hotmail.com>
2023-07-17 13:10:30 +0300

    created message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_093743) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_17_100324) do
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.text "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
