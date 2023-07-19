21f2e3c4c5674bc214331113f1850191e16b524a
gpanaretou <gpanaretou@hotmail.com>
2023-07-19 15:50:33 +0300

    added login/logout functionality and flash notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
 class SessionsController < ApplicationController
 
     def new
-        @user = User
+
     end
 
     def create 
-
+        user = User.find_by(username: params[:session][:username].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "There was something wrong with your credentials"
+            render 'new', status: :unprocessable_entity
+        end 
     end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to root_path
+    end
+
 end
